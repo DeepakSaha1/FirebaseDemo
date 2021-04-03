1885ae5bbf1481c77957c35e1f693332d84bc309
DeepakSaha1 <deepak.saha@tothenew.com>
2021-04-03 16:30:05 +0530

    firebase analytics commit

--- app/src/main/java/com/example/firebasedemo/MainActivity.kt
+++ app/src/main/java/com/example/firebasedemo/MainActivity.kt
@@ -6,25 +6,26 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.ktx.Firebase
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var firebaseCrashlytics: FirebaseCrashlytics
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
     val TAG = javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAnalytics = Firebase.analytics
 
         appPackageName()
 
-        //init
-        firebaseCrashlytics = FirebaseCrashlytics.getInstance()
-//        crashReportCustomize()
-        initUI()
+        findViewById<Button>(R.id.btn_track_event).setOnClickListener {
+            setUpUserProperty()
+            setUserId()
+            logEvent()
+        }
     }
 
     private fun appPackageName() {
@@ -32,45 +33,47 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, packageName)
     }
 
-    private fun initUI() {
-        findViewById<Button>(R.id.btn_crash).run {
-            setOnClickListener {
-                throw RuntimeException("Test crash")
-//                setUpCrash()
-//                setUpCrashForNonFatalException()
-            }
+    private fun logEvent() {
+        // suppose user click on ecommerce app for purchasing mobile
+        // firebase suggested event
+        val eventName = FirebaseAnalytics.Event.SELECT_ITEM
+        val bundle = Bundle().apply {
+            putInt(FirebaseAnalytics.Param.ITEM_ID, 784621)
+            putString(FirebaseAnalytics.Param.ITEM_BRAND, "ONE_PLUS")
+            putString("MODEL", "ONE_PLUS_8_PRO")
+            putDouble(FirebaseAnalytics.Param.PRICE, 39_499.00)
+            putBoolean("PURCHASE_BUTTON_CLICK", true)
+            putBoolean(FirebaseAnalytics.Param.SUCCESS, true)
         }
-    }
+        setDefaultParameters()
+        firebaseAnalytics.logEvent(eventName, bundle)
 
-    private fun setUpCrash() {
-        val array = arrayOf(1, 2, 3, 4)
-        array[5]
-    }
-
-    private fun setUpCrashForNonFatalException() {
-        try {
-            val a = 10
-            val b = 0
-            val someNum = a / b
-        } catch (e: ArithmeticException) {
-            firebaseCrashlytics.recordException(e)
-            Log.d(TAG, "Divided by zero operation cannot possible")
+        // custom event
+        val eventName2 = "share_image"
+        val bundle2 = Bundle().apply {
+            bundle.putString("image_name", "some_name")
+            bundle.putString("image_description", "some_text")
         }
+
+        firebaseAnalytics.logEvent(eventName2, bundle2)
     }
 
-    fun crashReportCustomize() {
-        //Add custom keys
-        firebaseCrashlytics.setCustomKey("DATE", getCurrentTime())
+    private fun setDefaultParameters() {
+        val parameters = Bundle().apply {
+            putString(FirebaseAnalytics.Param.LOCATION, "INDIA")
+            putString(FirebaseAnalytics.Param.CURRENCY, "INR")
+        }
 
-        //Add custom log messages
-        firebaseCrashlytics.log("ToTheNew trainee firebase crashlytics demo app")
-
-        //Set user identifiers
-        firebaseCrashlytics.setUserId("0123")
+        firebaseAnalytics.setDefaultEventParameters(parameters)
     }
 
-    private fun getCurrentTime(): String {
-        val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss", Locale.ENGLISH)
-        return sdf.format(Date())
+    private fun setUpUserProperty() {
+        firebaseAnalytics.setUserProperty("location", "INDIA")
+        firebaseAnalytics.setUserProperty("currency", "INR")
+        firebaseAnalytics.setUserProperty("is_subscribe", "false")
+    }
+
+    private fun setUserId() {
+        firebaseAnalytics.setUserId("64512")
     }
 }
